5deb0596fe618c378e261a36a120162f7881f5a0
Nate Ebel <n8ebel@goobar.dev>
2021-07-29 23:51:45 -0700

    Remove project sample repos

--- projects/ktor-sample/src/main/kotlin/dev/goobar/data/Joke.kt
+++
@@ -1,9 +0,0 @@
-package dev.goobar.data
-
-import kotlinx.serialization.Serializable
-
-@Serializable
-data class Joke(
-    val setup: String,
-    val punchline: String
-)
--- projects/react-sample/settings.gradle.kts
+++
@@ -1,3 +0,0 @@
-
-rootProject.name = "react-sample"
-
